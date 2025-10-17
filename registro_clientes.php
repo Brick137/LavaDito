3b27d0c2c06b74a944f8a9e4124b91c4b8d5ea4f
Brick137 <vazquezismael0713@gmail.com>
2025-10-16 22:17:48 -0600

    Guardo cambios

--- registro_clientes.php
+++ registro_clientes.php
@@ -33,7 +33,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LavaDito</title>
-    <link rel="stylesheet" href="registro.css">
+    <link rel="stylesheet" href="forms.css">
 </head>
 
 <body>
